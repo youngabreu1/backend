04060ea44b02887df7eae5fce782f2e1fa35b0ce
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2023-06-08 21:13:42 -0300

    primeiro commit branch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import { DataSource } from "typeorm"
+
 import express, {Request, Response} from 'express';
 
 const PORT = 3332;
+
+const PostgresDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "Abcdabcd",
+    database: "postgres",
+    entities: [
+        // ....
+    ],
+})
 
 const app = express();
 
